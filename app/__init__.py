e188c68ae55628fe1e3e31d388b840b1504f518a
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2023-11-05 13:28:09 -0600

    In progress

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 # These are my Imports
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# This imorts routes.py from the app folder which initializes all my routes in the app
-from app import routes
+
+# This initializes our extensions
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+# This imorts routes and models
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for,request
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug import url_parse
+
 
 @app.route('/')
 
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Dimitrius  '}
     posts = [
@@ -17,17 +22,35 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title = 'Home', user = user, posts = posts)
+    return render_template('index.html', title = 'Home', posts = posts)
 
 
-# This logs the user in and redirects them back to the home/index page
+# This is everything that is required in my login route
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    # This initializes the login form from my forms.py file.
     form = LoginForm()
+    # we are checking if the form is validating when you click submit. 
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form = form)
+        user = User.query.filter_by(username = form.username.data).first()
+        # This method is checking if the sign in works, if it doesnt it flashes a message and redirects them back to the login
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+            return redirect(next_page)
+        return redirect(url_for('index')) # If my code doesnt work I should delete this line of code and the one under it
+    return render_template('login.html', title = 'Sign In', form = form)
+        
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
